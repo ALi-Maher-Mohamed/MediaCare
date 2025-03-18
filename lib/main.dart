5d0488d5ad9fab76f1a141ce89c9f1085ef20fec
Ahmed Mansour <abydoes911@gmail.com>
2025-03-18 20:15:50 +0200

    Using Shared Preference instead of Secure Storage

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:media_care/core/SharedPref/shared_pref.dart';
 import 'package:media_care/presentation/views/Auth/login/login_view.dart';
 import 'package:media_care/presentation/views/Laboratories/data/repo/laporatory_repo_impl.dart';
 import 'observer.dart';
@@ -13,10 +13,8 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final service = LaboratoryRepoImpl(Dio());
   final result = await service.getLaboratories(page: 1);
-
   print('Test API call: ${result.fold((l) => l, (r) => r)}');
-  final secureStorage = const FlutterSecureStorage();
-  String? token = await secureStorage.read(key: "token");
+  String? token = await SharedPreference().getToken();
   Bloc.observer = MyBlocObserver();
 
   runApp(MediCare(
