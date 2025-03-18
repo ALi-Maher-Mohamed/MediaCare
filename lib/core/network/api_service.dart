c04b332f449f2f152a52fed6a8f97cd21b1d3ff6
AliMaher <ali.maher0013@gmail.com>
2025-03-18 02:18:26 +0200

    adding var base url to api service

--- lib/core/network/api_service.dart
+++ lib/core/network/api_service.dart
@@ -2,16 +2,20 @@ import 'package:dio/dio.dart';
 
 class ApiServiceFunctions {
   final Dio dio;
-  final baseUrl = 'http://192.168.1.4:8000';
+  final String baseUrl = 'http://10.0.2.2:8000';
   ApiServiceFunctions(this.dio);
   //Ali Maher BaseUrl --> http://192.168.1.4:8000
   //Ahmed Mansour BaseUrl --> http://10.0.2.2:8000
-  Future<Map<String, dynamic>> post(
-      {required String endpoint,
-      required Map<String, dynamic> data,
-      required Map<String, String> headers}) async {
-    var response = await dio.post('http://10.0.2.2:8000$endpoint',
-        data: data, options: Options(headers: {}));
+  Future<Map<String, dynamic>> post({
+    required String endpoint,
+    required Map<String, dynamic> data,
+    Map<String, dynamic>? headers,
+  }) async {
+    var response = await dio.post(
+      '$baseUrl$endpoint',
+      data: data,
+      options: Options(headers: headers),
+    );
     return response.data;
   }
 
@@ -22,7 +26,7 @@ class ApiServiceFunctions {
   }) async {
     try {
       final response = await dio.get(
-        'http://10.0.2.2:8000$endpoint', // Replace with your base URL
+        '$baseUrl$endpoint',
         queryParameters: queryParams,
         options: Options(headers: headers),
       );
