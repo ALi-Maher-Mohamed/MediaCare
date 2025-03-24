3bfcd3ada08c75f075f6498b9e2500187118f881
AliMaher <ali.maher0013@gmail.com>
2025-03-24 16:10:21 +0200

    adding native splash for android 12+ and ios done

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,7 @@ import 'package:media_care/presentation/views/AI_Feature/Labs_analytics/managers
 import 'package:media_care/presentation/views/AI_Feature/prescription_analysis/manager/cubit/prescription_cubit.dart';
 import 'package:media_care/presentation/views/AI_Feature/prescription_analysis/repo/Prescription_repo_impl.dart';
 import 'package:media_care/presentation/views/AI_Feature/symptom_analysis/managers/cubit/symptom_cubit.dart';
-import 'package:media_care/presentation/views/AI_Feature/symptom_analysis/repos/symptom_analysis_repo.dart';
 import 'package:media_care/presentation/views/AI_Feature/symptom_analysis/repos/symptom_analysis_repo_impl.dart';
-import 'package:media_care/presentation/views/Auth/login/login_view.dart';
 import 'package:media_care/presentation/views/Laboratories/data/repo/laporatory_repo_impl.dart';
 import 'observer.dart';
 import 'zoom_drawer.dart';
